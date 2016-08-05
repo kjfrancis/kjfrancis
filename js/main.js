cdc6da9656b37c7e1016bd0c0cee1a80b591c8fe
kjfrancis <kef2010@gmail.com>
2016-08-04 18:31:10 -0700

    changes6
    Updated javascript and added an Image Gallery to whole house, kitchen,
    and bathroom pages.

--- js/main.js
+++ js/main.js
@@ -11,3 +11,19 @@ function showDropDown () {
 function hideDropDown () {
 	$('#subNav').fadeOut(500);
 }
+
+
+// When the user clicks on .thumbNails img
+	// The selected image will show on the right without moving the thumbnail
+	$('.thumbNails img').on('click', function (e) {
+		e.preventDefault();
+
+	// Get the src attr from 'this' and store in var imageSrc
+		var imageSrc = $(this).attr('src');
+	// Find the element with the id cuurentImage and replace the src with imageSrc
+		console.log(imageSrc);
+		$('#currentImage').attr('src', imageSrc);
+	});
+
+
+
